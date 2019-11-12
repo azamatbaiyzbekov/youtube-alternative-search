d46d96158b27bb3c9e72667eb508bc894a2ac14c
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2019-11-11 22:04:41 -0800

    small updates

--- src/App.js
+++ src/App.js
@@ -26,14 +26,19 @@ class App extends Component {
     render() {
         return (
             <>
-            <nav>
+        <nav>
             <div class="nav-wrapper blue">
-              <div class="container">
-                <a href="#" class="brand-logo center">Youtube Search Alternative</a>
-      
-              </div>
+                <div class="container">
+                    <a href="#" class="brand-logo center">Youtube Search Alternative</a>
+                </div>
             </div>
-          </nav> 
+        </nav> 
+          <div className="card card-body mb-4 p-4">
+            <h1 className="display-4 text-center">
+                <i class="fa fa-youtube-square" aria-hidden="true"></i>
+            </h1>
+                <p className="lead text-center">Search for a videos</p>
+          </div>
             <div className='ui container' style={{marginTop: '1em'}}>
                 <SearchBar handleFormSubmit={this.handleSubmit}/>
                 <div className='ui grid'>
